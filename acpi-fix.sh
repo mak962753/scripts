aee56f1c5d3a76c237160b99b0ec506f37f4060a
mak962753 <mak962753@gmail.com>
2026-07-24 17:11:33 +0300

    acpi fix

--- acpi-fix.sh
+++ acpi-fix.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+# Disable ACPI GPEs that are storming (count > 5000, still enabled)
 
-while read -r I ; do
-    ITEM_PATH=$(echo $I | cut -f1 -d" ")
-    ITEM_COUNT=$(echo $I | cut -f2 -d" ")
-    if [[ $ITEM_COUNT -gt 5000 && "$ITEM_PATH" == *gpe6F ]]; then
-        echo "disabling gpe6F..." 
-        echo disable | sudo tee "$ITEM_PATH"
-    else 
-        echo "... $I"
-    fi
-done <<< "$(awk '$1>1000&&$4~/unmasked|enabled/&&FILENAME~/gpe[0-9A-F]{2}$/{print(FILENAME, $1, $4)}' /sys/firmware/acpi/interrupts/*)"
-
+awk '
+  $1 > 5000 && /[[:space:]]enabled[[:space:]]/ && !/disabled/ && FILENAME ~ /gpe[0-9A-F]{2}$/ {
+    print FILENAME, $1
+  }
+' /sys/firmware/acpi/interrupts/* | while read -r ITEM_PATH ITEM_COUNT; do
+  echo "disabling $(basename "$ITEM_PATH") (count=$ITEM_COUNT)..."
+  echo disable | sudo tee "$ITEM_PATH" >/dev/null
+done
